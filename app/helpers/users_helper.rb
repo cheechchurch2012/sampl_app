ae88ebe27f2287c3624f33e4b52d07bdb88e5602
Cheech Church <cheechchurch2012@yahoo.ie>
2012-06-24 18:13:57 +0100

    Finish sign in

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,5 @@
 module UsersHelper
+  
   # Returns the Gravatar (http://gravatar.com/) for the given user.
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
@@ -6,4 +7,3 @@ module UsersHelper
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
-  
